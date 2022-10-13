a1fc0f163606d1c9d58d0d1d3013020ca290b297
anil-bhusal <83385871+anil-bhusal@users.noreply.github.com>
2022-10-13 12:15:00 +0545

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -213,5 +213,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'anil@ismt.edu.np'
-EMAIL_HOST_PASSWORD = 'Sweetloli30@'
+EMAIL_HOST_USER = 'email@email.com'
+EMAIL_HOST_PASSWORD = 'password'
